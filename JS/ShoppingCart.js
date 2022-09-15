9f0d8cf6dbd5fdef5de0a97453424d1daded8fed
Mahim Choudhury <mahidul786@hotmail.co.uk>
2022-09-15 16:41:58 +0100

    completed shopping cart functionality

--- JS/ShoppingCart.js
+++ JS/ShoppingCart.js
@@ -1,14 +1,67 @@
 var quantity = 0;
+document.getElementById("hidden").style.display="none"
+document.getElementById("empty").style.display="block"
 
 function add(n){
   quantity += n;
   document.getElementById("quantity").innerHTML = quantity;
+  window.quantity = quantity;
+  console.log(quantity);
 }
 
 function remove(n){
   if(quantity > 0){
     quantity -= n;
     document.getElementById("quantity").innerHTML = quantity;
+    window.quantity = quantity;
+    console.log(quantity);
   }
 
 }
+
+var modal = document.getElementById("cartModal");
+
+var btn = document.getElementById("cartBtn");
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+function reset(){
+  quantity = 0;
+  window.quantity = quantity;
+  console.log(quantity)
+  document.getElementById("quantity").innerHTML = quantity;
+  updateCart()
+}
+
+function updateCart() {
+    if (quantity > 0){
+      document.getElementById("empty").style.display="none"
+      document.getElementById("cartQuantity").innerHTML = quantity;
+      var cost1 = quantity * 125
+      var USD = new Intl.NumberFormat("en-US",{
+        style:"currency",
+        currency:"USD"
+      })
+      var cost = USD.format(cost1)
+      console.log(cost)
+      document.getElementById("cost").innerHTML = cost;
+      document.getElementById("hidden").style.display="block"
+    } else if (quantity == 0){
+        document.getElementById("empty").style.display="block"
+        document.getElementById("hidden").style.display="none"
+    }
+  }
